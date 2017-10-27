9d7d6466e8a867dd52f7dbb9df10ccc6979bf0f5
Kumar Pratyush <Pratyush@thinkanalytics.in>
2017-10-27 06:43:38 +0530

    Updated DS fields

--- R/DS_Lib.R
+++ R/DS_Lib.R
@@ -1,6 +1,32 @@
 
 
 
-Basic = c('R',"SAS","Python","Tableu")
+DS_Base_PS = c('r',"sas","python","java","scala","javascript","angularjs","meteor","react")
+
+DS_DB_PS = c('sql',"nosql","couchdb","mongodb","json")
+
+DS_BD_PS = c('hadoop',"spark","pig","hive")
+
+DS_DV_PS = c('tableu',"qlik","matplotlib", "plot.ly", "c3.js", "dataiku", "highcharts")
+
+DS_ML_PS = c('machine learning',"scikit-learn","mllib",
+             "neural net", "deep learning", "glm", "regression",
+             "clustering","glmnet","svm","support vector machine","modelling")
+
+DS_IND = c("bfsi","banking","insurance","nbfc","financial services","fintech","lending")
+
+
+DS_BS = c("business analysis","multitask","prioritize","curious","innovation","initiative",
+          "problem solver","communication","stakeholder management")
+
+
+
+
+
+
+
+
+
+
 
 
